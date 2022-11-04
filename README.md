3761aa36ca9417a8aa9ea1878065fce34e4b97ec
Toderica Matei <namelessman26@gmail.com>
2022-11-04 19:10:46 +0200

    Created basic build system and testing infrastructure.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # proiectMCPP
+
+---
+
+Membri si numele nostru real:
+1. PerryVrajnitorincul -> Toderica Matei
+2. Codrut18 -> Plaiasu Codrut
+3. tibbbi -> Stan Tiberiu
+4. whathafakeisthis -> Visinovschi Vlad
---
+++ modules/Database/src/dbtest.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Perry on 11/7/2022.
+//
+
+#include<sqlite_orm/sqlite_orm.h>
+//This is the database modules and as such this makes sense imho.
+using namespace sqlite_orm;
+
+int main()
+{
+
+}
