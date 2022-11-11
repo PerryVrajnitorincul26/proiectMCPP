44a14f860f6eb9ad5e6820fe8fa87613a29da9c3
whathafakeisthis <visinovschivlad.bsn@gmail.com>
2022-11-11 16:46:36 +0200

    Created Movie class with additional functions
    Getters, Setters, Functions to remove and delete Movie's genre

---
+++ modules/UI/inc/Movie.h
@@ -0,0 +1,41 @@
+//
+// Created by Iusaq on 11/11/2022.
+//
+
+#ifndef MCPP_MOVIE_H
+#define MCPP_MOVIE_H
+
+
+#include <string>
+#include <array>
+#include <vector>
+#include <ostream>
+
+class Movie {
+    std::string m_title;
+    std::vector<std::string> m_genres;
+public:
+    Movie(const std::string &title, const std::vector<std::string> &genres);
+
+    Movie(const std::string &title, const std::string &str);
+
+    explicit Movie(const std::string &title);
+
+    Movie();
+
+    const std::string &getTitle() const;
+
+    void setTitle(const std::string &title);
+
+    const std::vector<std::string> &getGenres() const;
+
+    void addGenre(const std::string& genre);
+
+    void removeGenre(const std::string& genre);
+
+    ~Movie();
+
+};
+
+
+#endif //MCPP_MOVIE_H
---
+++ modules/UI/src/Movie.cpp
@@ -0,0 +1,75 @@
+//
+// Created by Iusaq on 11/11/2022.
+//
+
+#include "../inc/Movie.h"
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <iostream>
+
+///CONSTRUCTORS
+Movie::Movie()  = default; //default constructor
+
+Movie::Movie(const std::string &title) {
+    if (title.size() < 1)
+        throw std::invalid_argument("Title cannot be empty");
+    this->m_title=title;
+} //only title
+
+Movie::Movie(const std::string &title, const std::vector<std::string> &genres) : m_genres(genres) {
+    if (title.size() < 1)
+        throw std::invalid_argument("Title cannot be empty");
+    this->m_title=title;
+}  //only title a vector of genres
+
+Movie::Movie(const std::string &title, const std::string &genres) { //if the genres are received as a string, they are split by | delimiter
+    std::stringstream ss(genres);
+
+    if (title.size() < 1)
+        throw std::invalid_argument("Title cannot be empty");
+
+    this->m_title = title;
+
+    while(!ss.eof()){
+        std::string word;
+
+        std::getline(ss, word, '|');
+
+        if(word.size()>0)
+            this->m_genres.push_back(word);
+    }
+} //title and a string of genres delimited by '|'
+
+///GETTERS
+const std::string &Movie::getTitle() const {
+    return m_title;
+}
+
+const std::vector<std::string> &Movie::getGenres() const {
+    return m_genres;
+}
+
+///SETTERS
+void Movie::setTitle(const std::string &title) {
+    if(title.size()<1)
+        throw std::invalid_argument("Title cannot be empty");
+    m_title = title;
+}
+
+///DESTRUCTOR
+Movie::~Movie() {};
+
+///UTILITY FUNCTIONS
+void Movie::addGenre(const std::string &genre) {
+    if(genre.size()<1)
+        throw std::invalid_argument("Genre cannot be empty");
+    this->m_genres.push_back(genre);
+}
+
+void Movie::removeGenre(const std::string &genre){
+    this->m_genres.erase(std::remove(this->m_genres.begin(), this->m_genres.end(), genre), this->m_genres.end());
+}
+
+
