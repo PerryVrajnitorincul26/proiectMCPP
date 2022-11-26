63233c3c31516c00a17cc1986e8f3d2527b43aa5
whathafakeisthis <visinovschivlad.bsn@gmail.com>
2022-11-27 00:21:31 +0200

    Created a Message Label and made it work when searching a movie by title

--- modules/UI/src/moviestable.cpp
+++ modules/UI/src/moviestable.cpp
@@ -5,46 +5,53 @@
 #include <QString>
 
 MoviesTable::MoviesTable(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::MoviesTable)
-{
+        QDialog(parent),
+        ui(new Ui::MoviesTable) {
     ui->setupUi(this);
 }
 
-MoviesTable::~MoviesTable()
-{
+MoviesTable::~MoviesTable() {
     delete ui;
 }
 
-void MoviesTable::on_pushButton_clicked()
-{
+//NU VA ATINGETI DE FUNCTIA ASTA CA VA SPARG
+void MoviesTable::on_pushButton_clicked() {
 
     std::string titleToSearch = ui->titleInput->text().toStdString();
     std::string genreToSearch = ui->genreInput->text().toStdString();
-    QList<QString> moviesTitles;
-    QList<QString> moviesGenres;
+    QList <QString> moviesTitles;
+    QList <QString> moviesGenres;
 
-    QTableView *view = new QTableView;
+    QTableView * view = new QTableView;
     view->setEditTriggers(QAbstractItemView::NoEditTriggers);
 
     auto &dbRef = MovieDatabase::instance();
     auto searchResult = dbRef.searchMovieTitles(titleToSearch);
 
-    for(auto &item: *searchResult){
-        QString id = QString::number(item.m_movie_id);
-        QString title = QString::fromStdString(item.m_title);
-        QString genres = QString::fromStdString(item.m_genres);
-        moviesTitles.append(title);
-        moviesGenres.append(genres);
+    if (!searchResult->empty()) {
+        for (auto &item: *searchResult) {
+            QString id = QString::number(item.m_movie_id);
+            QString title = QString::fromStdString(item.m_title);
+            QString genres = QString::fromStdString(item.m_genres);
+            moviesTitles.append(title);
+            moviesGenres.append(genres);
+        }
+        MoviesTableModel *model = new MoviesTableModel(this);
+        model->populateData(moviesTitles, moviesGenres);
+
+        ui->tableView->setModel(model);
+
+        ui->tableView->horizontalHeader()->setVisible(true);
+        ui->tableView->show();
+        std::string message("We found ");
+        message.append(std::to_string(searchResult->size()));
+        message.append(" results");
+
+        ui->msgLabel->setText(QString::fromStdString(message));
     }
-
-    MoviesTableModel *model = new MoviesTableModel(this);
-    model->populateData(moviesTitles, moviesGenres);
-
-    ui->tableView->setModel(model);
-
-    ui->tableView->horizontalHeader()->setVisible(true);
-    ui->tableView->show();
+        else {
+        ui->msgLabel->setText("No results have been found!");
+    }
 }
 
 
