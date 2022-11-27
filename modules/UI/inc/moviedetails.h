040f6f18cfe044eeefd8e30c8ac191991504022f
Codrut18 <codrut.plaiasu35@gmail.com>
2022-11-27 19:31:48 +0200

    Added class "MovieDetails". Here the user will be able to give a review to the movie and also here will be other details for the movie.

---
+++ modules/UI/inc/moviedetails.h
@@ -0,0 +1,28 @@
+//
+// Created by Codrut on 11/27/2022.
+//
+
+#ifndef MCPP_MOVIEDETAILS_H
+#define MCPP_MOVIEDETAILS_H
+
+#include <QWidget>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MovieDetails; }
+QT_END_NAMESPACE
+
+class MovieDetails : public QWidget {
+Q_OBJECT
+
+public:
+    explicit MovieDetails(QWidget *parent = nullptr);
+
+    ~MovieDetails() override;
+
+private:
+    Ui::MovieDetails *ui;
+};
+
+
+#endif //MCPP_MOVIEDETAILS_H
---
+++ modules/UI/src/moviedetails.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Codrut on 11/27/2022.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_MovieDetails.h" resolved
+
+#include "moviedetails.h"
+#include "ui_MovieDetails.h"
+
+
+MovieDetails::MovieDetails(QWidget *parent) :
+        QWidget(parent), ui(new Ui::MovieDetails) {
+    ui->setupUi(this);
+}
+
+MovieDetails::~MovieDetails() {
+    delete ui;
+}
