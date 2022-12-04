a22139509ddd83f74e2b04c4c72d416059f69802
whathafakeisthis <visinovschivlad.bsn@gmail.com>
2022-12-04 21:47:44 +0200

    Created a MovieDTO Class to get data about movies from TMDB Api

--- modules/UI/src/showmovie.cpp
+++ modules/UI/src/showmovie.cpp
@@ -7,11 +7,7 @@ ShowMovie::ShowMovie(QWidget *parent) :
     ui(new Ui::ShowMovie)
 {
     ui->setupUi(this);
-    QPixmap pix("C:/Users/Bulibas/Documents/GitHub/proiectMCPP/modules/UI/moviedatabase.jpg");
-    ui->label_Image->setPixmap(pix.scaled(300,300,Qt::KeepAspectRatio));
-    ui->label_movieTitle->setText("Title");
-    ui->label_Genre->setText("All genre");
-    ui->label_rating->setText("Rating from 1 to 5");
+
 }
 
 ShowMovie::~ShowMovie()
